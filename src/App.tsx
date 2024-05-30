0f030f5dae195339f093ba32d2a8dc5eb2a26c53
bcx <femolak@gmail.com>
2024-05-30 18:50:22 +0200

    updates to error lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import ProtectedRoute from "./components/auth/ProtectedRouter";
 import Callback from "./pages/Callback";
 import axiosInstance from "./utils/axios";
-import { handleAxiosError } from "./errors/axioxErrorHandler";
-import { useState } from "react";
-import ErrorPage from "./pages/ErrorPage";
-import { ErrorProvider } from "./errors/ErrorContext";
-import ErrorBoundary from "./errors/ErrorBoundary";
+import ErrorHandler from "./components/ErrorComponent";
 
 const App = () => {
-  const [error, setError] = useState<{ code: number; message: string } | null>(
-    null
-  );
   const buildQueryParameters = (
     filters: Record<string, string | undefined>
   ) => {
@@ -39,59 +32,64 @@ const App = () => {
     return queryString.toString();
   };
 
-  const saveJobHandler = async (newJob: any) => {
+  const saveJobHandler = async (newJob) => {
     try {
       const res = await axiosInstance.post("/jobs", newJob);
       return res.data;
     } catch (error) {
-      setError({ code: error.response?.status, message: error.message });
-      throw error;
+      throw new Response(error.response?.data?.message || error.message, {
+        status: error.response?.status || 500,
+      });
     }
   };
 
-  const deleteJob = async (id: string) => {
+  const deleteJob = async (id) => {
     try {
       const res = await axiosInstance.delete(`/jobs/${id}`);
       return res.data;
     } catch (error) {
-      setError({ code: error.response?.status, message: error.message });
-      throw error;
+      throw new Response(error.response?.data?.message || error.message, {
+        status: error.response?.status || 500,
+      });
     }
   };
 
-  const updateJob = async (job: any) => {
+  const updateJob = async (job) => {
     try {
       const res = await axiosInstance.put(`/jobs/${job.id}`, job);
       return res.data;
     } catch (error) {
-      setError({ code: error.response?.status, message: error.message });
-      throw error;
+      throw new Response(error.response?.data?.message || error.message, {
+        status: error.response?.status || 500,
+      });
     }
   };
 
-  const searchJob = async (filters: {
-    type: string;
-    location: string;
-    title: string;
-  }) => {
+  const searchJob = async (filters) => {
     console.log(filters);
     try {
       const url = `/jobs/search?${buildQueryParameters(filters)}`;
       const res = await axiosInstance.get(url);
       return res.data;
     } catch (error) {
-      setError({ code: error.response?.status, message: error.message });
-      throw error;
+      throw new Response(error.response?.data?.message || error.message, {
+        status: error.response?.status || 500,
+      });
     }
   };
 
   const fetchJobs = async () => {
     try {
+      console.log("here");
       const res = await axiosInstance.get("/jobs");
+      console.log(res.data);
       return res.data;
     } catch (error) {
-      setError({ code: error.response?.status, message: error.message });
-      throw error;
+      console.log("Got Error in Loader: ", JSON.stringify(error, null, 2));
+      // throw new Response(error.response?.data?.message || error.message, {
+      //   status: error.response?.status || 500,
+      // });
+      throw new Error("Network Error: technical issues from us");
     }
   };
 
@@ -107,10 +105,6 @@ const App = () => {
     logout({ logoutParams: { returnTo: window.location.origin } });
   };
 
-  if (error) {
-    return <ErrorPage errorCode={error.code} errorMessage={error.message} />;
-  }
-
   const RoutesJSX = (
     <Route
       path="/"
@@ -122,23 +116,19 @@ const App = () => {
           logout={logoutFxn}
         />
       }
+      errorElement={<ErrorHandler />}
     >
       <Route index element={<HomePage data={fetchJobs} />} />
-      <Route path="jobs" element={<JobListing data={fetchJobs} />} />
+      <Route
+        path="jobs"
+        element={<JobListing data={fetchJobs} />}
+        errorElement={<ErrorHandler />}
+      />
       <Route
         path="jobs/search"
         element={<JobSearchResults data={fetchJobs} />}
       />
       <Route path="callback" element={<Callback />} />
-      <Route
-        path="*"
-        element={
-          <ErrorPage
-            errorCode={404}
-            errorMessage="Page Not Found or Does Not Exists"
-          />
-        }
-      />
       <Route
         path="/jobs/:id"
         element={
@@ -164,13 +154,7 @@ const App = () => {
   );
 
   const router = createBrowserRouter(createRoutesFromElements(RoutesJSX));
-  return (
-    <ErrorProvider>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
-    </ErrorProvider>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;

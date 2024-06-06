f7bc384f1f4376c24466c0ac34015fe7d9857244
bcx <femolak@gmail.com>
2024-06-06 13:51:51 +0200

    backend server env var fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-console.log(import.meta.env.VITE_BACKEND_URL)
-// https://vitejs.dev/config/
+import { config } from 'dotenv';
+config();
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:3002',
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },

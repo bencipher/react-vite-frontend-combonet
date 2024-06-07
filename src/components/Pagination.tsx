f0d2f700294bddbebfb8effcfe2d704678dbe36d
bcx <femolak@gmail.com>
2024-06-07 14:16:32 +0200

    opaginate nav

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -18,15 +18,16 @@ const Pagination = ({
     <div className="pt-10 text-center">
       <nav aria-label="Page navigation example">
         <ul className="inline-flex -space-x-px text-base h-10">
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              Previous
-            </a>
-          </li>
-
+          {currentPage > 1 && (
+            <li>
+              <a
+                href="#"
+                className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Previous
+              </a>
+            </li>
+          )}
           {pages.map((page, index) => {
             return (
               <li key={index}>
@@ -44,14 +45,16 @@ const Pagination = ({
               </li>
             );
           })}
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              Next
-            </a>
-          </li>
+          {currentPage < pages.length && (
+            <li>
+              <a
+                href="#"
+                className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Next
+              </a>
+            </li>
+          )}
         </ul>
       </nav>
     </div>

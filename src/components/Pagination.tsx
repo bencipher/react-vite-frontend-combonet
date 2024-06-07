6aa759ae5fa2420c3b2fb2e75224be9490005f6e
bcx <femolak@gmail.com>
2024-06-07 20:20:23 +0200

    pagination style

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -24,6 +24,7 @@ const Pagination = ({
           {currentPage > 1 && (
             <li>
               <a
+                onClick={() => setCurrentPage((currentPage) => currentPage - 1)}
                 href="#"
                 className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               >
@@ -51,6 +52,7 @@ const Pagination = ({
           {currentPage < pages.length && (
             <li>
               <a
+                onClick={() => setCurrentPage((currentPage) => currentPage + 1)}
                 href="#"
                 className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               >

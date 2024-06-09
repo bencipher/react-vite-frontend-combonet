06d2735f1f08d3d36721ae6370636ccab7046ea0
bcx <femolak@gmail.com>
2024-06-09 10:20:55 +0200

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import "./index.css";
-import { Auth0ProviderWithNavigate } from "./contexts/auth0-provider-with-navigate.tsx";
+import { Auth0ProviderWithNavigate } from "./contexts/Auth0ProviderWithNavigate.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
